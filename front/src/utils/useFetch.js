24b5c281bb47d17b002bcd63ef25f7da42dcea20
Hugo Robitaillie <hugo@la-ligne-web.com>
2022-09-30 17:55:34 +0200

    update au travail

---
+++ front/src/utils/useFetch.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const useFetch = (prop) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    // au chargement de la fonction récupérer les données
+    useEffect(()=> {
+        const fetchData = async () => {
+            setLoading(true);
+            const result = await axios(import.meta.env.VITE_API_URL + prop);
+            const data = result.data
+            console.log(data);
+        }
+        fetchData()
+    }, [])
+
+    return { loading, error, data }
+}
+
+export default useFetch;
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "node:path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: { 
-      '@components': path.resolve(__dirname, './src/components'), 
-      '@img': path.resolve(__dirname, './src/assets/img'), 
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@img": path.resolve(__dirname, "./src/assets/img"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
   // ...
-  plugins: [react()]
-})
+  plugins: [react()],
+});
